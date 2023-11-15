4b60592151c52cbb9d6727d3e9d6123b1ee01561
Alexandre Araújo Costa <alexandre.araujo.costa@gmail.com>
2023-11-15 20:07:51 -0300

    Add files via upload

---
+++ junta dados.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 18:16:55 2023
+
+@author: Alexandre Araújo Costa
+"""
+
+import pandas as pd
+
+arquivo_retornar = 'total.csv'
+
+source_list = ['processos_julgados_virtual_TP500.txt',
+             'processos_julgados_virtual_TP1000.txt',
+             'processos_julgados_virtual_TP1500.txt',
+             'processos_julgados_virtual_TP2000.txt',
+             'processos_julgados_virtual_TP2500.txt',
+             'processos_julgados_virtual_TP3000.txt',
+             'processos_julgados_virtual_TP3500.txt',
+             'processos_julgados_virtual_TP4000.txt',
+             'processos_julgados_virtual_TP4500.txt',
+             'processos_julgados_virtual_TP5000.txt',
+             'processos_julgados_virtual_TP5500.txt',
+             'processos_julgados_virtual_TP6000.txt',
+             'processos_julgados_virtual_TP_final.txt',
+             
+             ]
+
+base = pd.read_csv(source_list[0], dtype={"teste": str})
+
+for n in range(len(source_list)-1):
+    item = source_list[n-1]
+    dados = pd.read_csv(item, dtype={"teste": str})
+    base = pd.concat([base,dados])
+
+
+base.to_csv(arquivo_retornar, index=False)
